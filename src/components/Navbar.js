1f2edf52089076de23f05d383257c3c3deeecda9
shikhar raj <shikhajjndjd@gmail.com>
2024-07-30 13:07:52 +0530

    new app

---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+export default function Navbar(Props) {
+
+ const [MyStyle,setMyStyle] = useState({
+    color: 'white',
+    backgroundColor: 'black', 
+  })
+
+  const mode = ()=> {
+    console.log("mode")
+   if (MyStyle.color == 'white') {
+    setMyStyle({
+      color: 'black',
+      backgroundColor: 'white', 
+    })    
+   }
+   else( 
+    setMyStyle({
+      color: 'white',
+      backgroundColor: 'black', 
+    })    
+   )
+    }
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light" style={MyStyle}>
+        <div className="container-fluid" >
+          <a className="navbar-brand" href="#" style={MyStyle}>{Props.text}</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+              <li className="nav-item" >
+                <a className="nav-link active" aria-current="page" href="#" style={MyStyle}>Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" style={MyStyle}>Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" style={MyStyle} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" style={MyStyle}>
+                  <li><a className="dropdown-item"style={MyStyle} href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#"style={MyStyle}>Another action</a></li>
+                  <li><hr className="dropdown-divider" style={MyStyle}/></li>
+                  <li><a className="dropdown-item" href="#"style={MyStyle}>Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <button className="btn btn-outline-primary" onClick={mode} style={MyStyle}> Enable light mode </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
---
+++ src/components/Textarea.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+export default function Textarea(Props) {
+    const handleUpClick =()=>{
+        console.log("upper case was clicked" + text)
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+    const handleloClick =()=>{
+        console.log("upper case was clicked" + text)
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+    const handleOnChange =(event)=>{
+        console.log("on change",event.target.value)
+        setText(event.target.value)
+    }
+    const handleclearClick =(event)=>{
+        console.log("on change",event.target.value)
+        setText("")
+    }
+    const [text, setText] = useState('Enter text here')
+    return (
+        <>
+                <div className="mb-3">
+                    <h1 >{Props.heading}</h1>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <div className="buttons">
+                <button className='btn btn-primary m-1' onClick={handleUpClick}> convert to uppercase </button>
+                <button className='btn btn-primary m-1' onClick={handleloClick}> convert to lowercase </button>
+                <button className='btn btn-primary m-1' onClick={handleclearClick}> clear text </button>
+                </div>
+
+                <div className="container my-3">
+                    <h1>
+                    Your Text Summary
+                    </h1>
+                    <p>{text.split(" ").length}  words and {text.length} characters </p>
+                    <p>{0.008 *text.split(" ").length } miniutes to read the above text </p>
+                <h2>
+                    Preview 
+                </h2>
+                <p>
+                    {text}
+                </p>
+                </div>
+        </>
+    )
+}
